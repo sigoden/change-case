aa91fd7e9b8f83cd035d0facd630e7b65b831b28
sigoden <sigoden@gmail.com>
2020-09-09 20:15:42 +0800

    update readme and add github ci

--- README.md
+++ README.md
@@ -1,9 +1,31 @@
 # Change Case
 
 [![crates.io](https://img.shields.io/crates/v/change-case.svg)](https://crates.io/crates/change-case)
-[![Documentation](https://docs.rs/change-case/badge.svg)][crate-docs]
+[![Documentation](https://docs.rs/change-case/badge.svg)](https://docs.rs/change-case)
 
-> Transform a string between `camelCase`, `PascalCase`, `Capital Case`, `snake_case`, `param-case`, `CONSTANT_CASE` and others.
+Transform a string between `camelCase`, `PascalCase`, `Capital Case`, `snake_case`, `param-case`, `CONSTANT_CASE` and others.
+
+## Usage
+
+Add this to your `Cargo.toml`:
+
+```toml
+[dependencies]
+change-case = "0.1"
+```
+
+```rust
+use change_case::*;
+
+assert_eq!(pascal_case("test string"), "TestString");
+assert_eq!(camel_case("Test String"), "testString");
+assert_eq!(captial_case("test string"), "Test String");
+assert_eq!(snake_case("Test String"), "test_string");
+assert_eq!(constant_case("test string"), "TEST_STRING");
+assert_eq!(constant_case("test string"), "TEST_STRING");
+assert_eq!(param_case("test string"), "test-string");
+```
+
 
 ## License
 
--- src/title_case.rs
+++ src/title_case.rs
@@ -31,7 +31,8 @@ pub fn title_case(input: &str) -> String {
             (input.chars().nth(index2).map_or(true, |v| v != ':') ||
                 input.chars().nth(index2 + 1).map_or(false, |v| RE_WHITESPACE.is_match(v.to_string().as_str())))
         {
-            let new_token = RE_ALPHANUMERIC.replace(token, |v: &Captures| format!("{}", &v[0].to_uppercase()));
+            let new_token =
+                RE_ALPHANUMERIC.replace(token, |v: &Captures| format!("{}", &v[0].to_uppercase()));
             result.push_str(new_token.as_ref())
         } else {
             result.push_str(token)
@@ -53,36 +54,90 @@ mod tests {
         assert_eq!(title_case("one. two."), "One. Two.");
         assert_eq!(title_case("a small word starts"), "A Small Word Starts");
         assert_eq!(title_case("small word ends on"), "Small Word Ends On");
-        assert_eq!(title_case("we keep NASA capitalized"), "We Keep NASA Capitalized");
-        assert_eq!(title_case("pass camelCase through"), "Pass camelCase Through");
-        assert_eq!(title_case("follow step-by-step instructions"), "Follow Step-by-Step Instructions");
-        assert_eq!(title_case("your hair[cut] looks (nice)"), "Your Hair[cut] Looks (Nice)");
+        assert_eq!(
+            title_case("we keep NASA capitalized"),
+            "We Keep NASA Capitalized"
+        );
+        assert_eq!(
+            title_case("pass camelCase through"),
+            "Pass camelCase Through"
+        );
+        assert_eq!(
+            title_case("follow step-by-step instructions"),
+            "Follow Step-by-Step Instructions"
+        );
+        assert_eq!(
+            title_case("your hair[cut] looks (nice)"),
+            "Your Hair[cut] Looks (Nice)"
+        );
         assert_eq!(title_case("leave Q&A unscathed"), "Leave Q&A Unscathed");
-        assert_eq!(title_case("piña colada while you listen to ænima"), "Piña Colada While You Listen to Ænima");
-        assert_eq!(title_case("start title – end title"), "Start Title – End Title");
+        assert_eq!(
+            title_case("piña colada while you listen to ænima"),
+            "Piña Colada While You Listen to Ænima"
+        );
+        assert_eq!(
+            title_case("start title – end title"),
+            "Start Title – End Title"
+        );
         assert_eq!(title_case("start title–end title"), "Start Title–End Title");
-        assert_eq!(title_case("start title — end title"), "Start Title — End Title");
+        assert_eq!(
+            title_case("start title — end title"),
+            "Start Title — End Title"
+        );
         assert_eq!(title_case("start title—end title"), "Start Title—End Title");
-        assert_eq!(title_case("start title - end title"), "Start Title - End Title");
+        assert_eq!(
+            title_case("start title - end title"),
+            "Start Title - End Title"
+        );
         assert_eq!(title_case("don't break"), "Don't Break");
         assert_eq!(title_case(r#""double quotes""#), r#""Double Quotes""#);
-        assert_eq!(title_case(r#"double quotes "inner" word"#), r#"Double Quotes "Inner" Word"#);
-        assert_eq!(title_case("fancy double quotes “inner” word"), "Fancy Double Quotes “Inner” Word");
-        assert_eq!(title_case("have you read “The Lottery”?"), "Have You Read “The Lottery”?");
+        assert_eq!(
+            title_case(r#"double quotes "inner" word"#),
+            r#"Double Quotes "Inner" Word"#
+        );
+        assert_eq!(
+            title_case("fancy double quotes “inner” word"),
+            "Fancy Double Quotes “Inner” Word"
+        );
+        assert_eq!(
+            title_case("have you read “The Lottery”?"),
+            "Have You Read “The Lottery”?"
+        );
         assert_eq!(title_case("one: two"), "One: Two");
         assert_eq!(title_case("one two: three four"), "One Two: Three Four");
-        assert_eq!(title_case(r#"one two: "Three Four""#), r#"One Two: "Three Four""#);
-        assert_eq!(title_case("email email@example.com address"), "Email email@example.com Address");
-        assert_eq!(title_case("you have an https://example.com/ title"), "You Have an https://example.com/ Title");
-        assert_eq!(title_case("_underscores around words_"), "_Underscores Around Words_");
-        assert_eq!(title_case("*asterisks around words*"), "*Asterisks Around Words*");
+        assert_eq!(
+            title_case(r#"one two: "Three Four""#),
+            r#"One Two: "Three Four""#
+        );
+        assert_eq!(
+            title_case("email email@example.com address"),
+            "Email email@example.com Address"
+        );
+        assert_eq!(
+            title_case("you have an https://example.com/ title"),
+            "You Have an https://example.com/ Title"
+        );
+        assert_eq!(
+            title_case("_underscores around words_"),
+            "_Underscores Around Words_"
+        );
+        assert_eq!(
+            title_case("*asterisks around words*"),
+            "*Asterisks Around Words*"
+        );
         assert_eq!(title_case("this vs. that"), "This vs. That");
         assert_eq!(title_case("this vs that"), "This vs That");
         assert_eq!(title_case("this v. that"), "This v. That");
         assert_eq!(title_case("this v that"), "This v That");
-        assert_eq!(title_case("Scott Moritz and TheStreet.com’s million iPhone la-la land"), "Scott Moritz and TheStreet.com’s Million iPhone La-La Land");
+        assert_eq!(
+            title_case("Scott Moritz and TheStreet.com’s million iPhone la-la land"),
+            "Scott Moritz and TheStreet.com’s Million iPhone La-La Land"
+        );
         assert_eq!(title_case("Notes and observations regarding Apple’s announcements from ‘The Beat Goes On’ special event"), "Notes and Observations Regarding Apple’s Announcements From ‘The Beat Goes On’ Special Event");
-        assert_eq!(title_case("the quick brown fox jumps over the lazy dog"), "The Quick Brown Fox Jumps over the Lazy Dog");
+        assert_eq!(
+            title_case("the quick brown fox jumps over the lazy dog"),
+            "The Quick Brown Fox Jumps over the Lazy Dog"
+        );
         assert_eq!(title_case("newcastle upon tyne"), "Newcastle upon Tyne");
         assert_eq!(title_case("newcastle *upon* tyne"), "Newcastle *upon* Tyne");
     }
